5e087129ff28cc22aebfc1a87d32224cd6a5044c
xxb <769289019@qq.com>
2021-12-06 13:48:02 +0800

    feat: mybatisplus 简单模块

---
+++ mybatisplus/src/main/java/com/example/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.example.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.mybatisplus.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+
+}
---
+++ mybatisplus/src/test/java/com/example/mybatisplus/mapper/UserMapperTest.java
@@ -0,0 +1,23 @@
+package com.example.mybatisplus.mapper;
+
+
+import com.example.mybatisplus.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class UserMapperTest {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        userList.forEach(System.out::println);
+    }
+}
