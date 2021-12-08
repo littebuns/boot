1674a5d342e1384593c72d120554c4bb384bc87e
xxb <769289019@qq.com>
2021-12-08 15:54:45 +0800

    style: 依赖，包结构修改

--- mybatisplus/src/test/java/com/example/mybatisplus/mapper/UserMapperTest.java
+++ mybatisplus/src/test/java/com/example/mybatisplus/mapper/UserMapperTest.java
@@ -2,11 +2,14 @@ package com.example.mybatisplus.mapper;
 
 
 import com.example.mybatisplus.User;
+import org.assertj.core.util.Maps;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class UserMapperTest {
@@ -20,4 +23,25 @@ class UserMapperTest {
         List<User> userList = userMapper.selectList(null);
         userList.forEach(System.out::println);
     }
+
+    /**
+     * 批量查询
+     */
+    @Test
+    public void t1(){
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1, 2, 3));
+        users.forEach(System.out::println);
+    }
+
+    /**
+     * 条件查询
+     */
+    @Test
+    public void t2(){
+        Map<String, Object> map = Maps.newHashMap("name", "xxb");
+        map.put("id", 1);
+        userMapper.selectByMap(map).forEach(System.out::println);
+    }
+
+
 }
